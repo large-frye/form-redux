25d61a2aeb9b694ea4db54d38295a9f7dfa9e77d
Andrew Frye <a.frye4@gmail.com>
2018-03-24 20:59:13 -0400

    adding reducer + action example

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,53 @@
-let nextTodoId = 0;
+type SubReddit = { item: string };
 
-interface ISimpleTodo { type: String, id: Number, text: String };
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
 
-export function addTodo(text: String): ISimpleTodo {
-  return {
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text
+export const selectSubreddit = (subreddit: SubReddit) => ({
+  type: SELECT_SUBREDDIT,
+  subreddit
+});
+
+export const invalidateSubreddit = (subreddit: SubReddit) => ({
+  type: INVALIDATE_SUBREDDIT,
+  subreddit
+});
+
+export const requestPosts = (subreddit: SubReddit) => ({
+  type: REQUEST_POSTS,
+  subreddit
+});
+
+export const receivePosts = (subreddit: SubReddit, json: any) => ({
+  type: RECEIVE_POSTS,
+  subreddit,
+  posts: json.data.children.map((child: any) => child.data),
+  receivedAt: Date.now()
+});
+
+const fetchPosts = (subreddit: SubReddit) => (dispatch: any) => {
+  dispatch(requestPosts(subreddit))
+  return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+  .then((response: any) => response.json())
+  .then((json) => dispatch(receivePosts(subreddit, json)));
+}
+
+const shouldFetchPosts = (state: any, subreddit: any) => {
+  const posts = state.postsBySubreddit[subreddit];
+  if (!posts) {
+    return true;
+  }
+  if (posts.isFetching) {
+    return false;
+  }
+  return posts.didInvalidate;
+}
+
+export const fetchPostsIfNeeded = (subreddit: SubReddit) => (dispatch: any, getState: any) => {
+  if (shouldFetchPosts(getState(), subreddit)) {
+    return dispatch(fetchPosts(subreddit));
   }
 }
+
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,64 @@
 import { combineReducers } from 'redux';
-import todos from './todos';
-import visibilityFilter from './visibilityFilters';
+import {
+  SELECT_SUBREDDIT, INVALIDATE_SUBREDDIT, RECEIVE_POSTS, REQUEST_POSTS
+} from '../actions';
 
-const ReducersApp = combineReducers({
-  todos,
-  visibilityFilter
+const selectedSubreddit = (state = 'reactjs', action: any) => {
+  switch(action.type) {
+    case SELECT_SUBREDDIT:
+      return action.subreddit;
+    default:
+      return state;
+  }
+}
+
+const posts = (state = {
+  isFetching: false,
+  didInvalidate: false,
+  items: <Array<any>>[]
+}, action: any) => {
+  switch (action.type) {
+    case INVALIDATE_SUBREDDIT:
+      return {
+        ...state,
+        didInvalidate: true
+      }
+    case REQUEST_POSTS:
+      return {
+        ...state,
+        isFetching: true,
+        didInvalidate: true
+      }
+    case RECEIVE_POSTS:
+      return {
+        ...state,
+        isFetching: false,
+        didInvalidate: false,
+        items: action.posts,
+        lastUpdated: action.receivedAt
+      }
+    default:
+      return state
+  }
+}
+
+const postsBySubreddit = (state: any = {}, action: any) => {
+  switch (action.type) {
+    case INVALIDATE_SUBREDDIT:
+    case RECEIVE_POSTS:
+    case REQUEST_POSTS:
+      return {
+        ...state,
+        [action.subreddit]: posts(state[action.subreddit], action)
+      }
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  postsBySubreddit,
+  selectedSubreddit
 });
 
-export default ReducersApp;
+export default rootReducer;
